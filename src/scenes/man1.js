14b8c7f01bfbb46af107ae35d3d26834d984beaf
huyenpt21 <huyenpthe140045@fpt.edu.vn>
2021-10-28 22:34:36 +0700

    upcode by huyen

--- src/scenes/man1.js
+++ src/scenes/man1.js
@@ -1,54 +1,76 @@
 class Man1 extends Phaser.Scene {
 
-	constructor() {
-		super("Man1");
+    constructor() {
+        super("Man1");
 
-	}
-    preload(){
-        this.load.image('background','assets/1.png');
-        this.load.image('background_1','assets/2.png');
-        this.load.image('back_khai','assets/7.png');
-        this.load.audio('khai_audio','assets/khai.m4a');
     }
-    create(){
-        
-        this.background=this.add.image(0,0,'background').setOrigin(0,0);
-        this.background_1=this.add.image(0,0,'background_1').setOrigin(0,0).setVisible(false);
-        this.back_text=this.add.text(40,50,"BACK",{fontSize:54,color:"#FFFFFF"}).setVisible(false);
-        this.back_khai=this.add.image(207,211,'back_khai').setOrigin(0,0).setScale(34/75).setVisible(false);
-        this.khai_audio=this.sound.add('khai_audio',{loop:false});
+    preload() {
+        this.load.image('background', 'assets/1.png');
+        this.load.image('background_1', 'assets/2.png');
+        this.load.image('back_khai', 'assets/7.png');
+        this.load.audio('khai_audio', 'assets/khai.m4a');
+        this.load.image('back_huyen', 'assets/5.png');
+        this.load.audio('audio_huyen', 'assets/huyen.mp3');
+    }
+    create() {
+        this.background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        this.background_1 = this.add.image(0, 0, 'background_1').setOrigin(0, 0).setVisible(false);
+        this.back_text = this.add.text(40, 50, "BACK", { fontSize: 54, color: "#FFFFFF" }).setVisible(false);
+        this.back_khai = this.add.image(207, 211, 'back_khai').setOrigin(0, 0).setScale(34 / 75).setVisible(false);
+        this.khai_audio = this.sound.add('khai_audio', { loop: false });
         this.background.setInteractive();
-        this.po_x=0;
-        this.po_y=0;
+        this.po_x = 0;
+        this.po_y = 0;
         this.background.on('pointerdown', () => {
-            this.po_x=this.game.input.mousePointer.x;
-            this.po_y=this.game.input.mousePointer.y;
-            if(this.po_x>=441&&this.po_x<=583&&this.po_y>=259&&this.po_y<=398){
+            this.po_x = this.game.input.mousePointer.x;
+            this.po_y = this.game.input.mousePointer.y;
+            if (this.po_x >= 441 && this.po_x <= 583 && this.po_y >= 259 && this.po_y <= 398) {
                 this.background_1.setVisible(true);
                 this.back_text.setVisible(true);
                 this.back_khai.setVisible(true);
                 this.khai_audio.play();
             }
-       
+
         })
         this.back_text.setInteractive();
-        this.back_text.on('pointerdown',()=>{
+        this.back_text.on('pointerdown', () => {
             this.back_text.setVisible(false);
             this.background_1.setVisible(false);
             this.back_khai.setVisible(false);
         })
-        
-  
+
+        // ----------Huyen--------------
+        this.back_huyen = this.add.image(207, 211, 'back_huyen').setOrigin(0, 0).setScale(34 / 75).setVisible(false);
+        this.audio_huyen = this.sound.add('audio_huyen', { loop: false });
+        this.add_image(this.back_huyen, this.audio_huyen, 42, 220, 406, 581);
     }
-    update(){
+    
+    update() {
         // if(this.game.input.mousePointer.isDown){
         //     this.background_1==
-            
-            
+
+
         // }
+    }
 
-        
+    add_image(image, audio, X_min, X_max, Y_min, Y_max) {
+        this.background.on('pointerdown', () => {
+            this.po_x = this.game.input.mousePointer.x;
+            this.po_y = this.game.input.mousePointer.y;
+            if (this.po_x >= X_min && this.po_x <= X_max && this.po_y >= Y_min && this.po_y <= Y_max) {
+                this.background_1.setVisible(true);
+                this.back_text.setVisible(true);
+                image.setVisible(true);
+                audio.play();
+            }
 
-
+        })
+        this.back_text.setInteractive();
+        this.back_text.on('pointerdown', () => {
+            this.back_text.setVisible(false);
+            this.background_1.setVisible(false);
+            image.setVisible(false);
+            audio.stop();
+        })
     }
 }
