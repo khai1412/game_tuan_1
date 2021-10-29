24e2b00ea55a7c0dd15b4127efe6f13126c4f191
khai1412 <khai1412@gmail.com>
2021-10-29 23:58:22 +0700

    sua phan giao dien man 2

--- src/scenes/man2.js
+++ src/scenes/man2.js
@@ -13,14 +13,25 @@ class man2 extends Phaser.Scene {
        this.load.spritesheet('player1','assets/dude.png',{frameWidth:32,frameHeight:48});
     }
     create(){
-       this.add.image(0,0,'main_background').setOrigin(0,0).setScale(375/512,375/256).setVisible(true);
-       this.add.image(0,0,'blur_background').setOrigin(0,0).setScale(2/3,375/374).setVisible(false);
-       this.add.image(5, 120, 'frame_question').setOrigin(0, 0).setScale(37/30,250/183).setVisible(false);
+       this.main_background=this.add.image(0,0,'main_background').setOrigin(0,0).setScale(375/512,375/256).setVisible(true);
+       this.blur_background=this.add.image(0,0,'blur_background').setOrigin(0,0).setScale(2/3,375/374).setVisible(false);
+       this.frame_question=this.add.image(5, 120, 'frame_question').setOrigin(0, 0).setScale(37/30,250/183).setVisible(false);
+       this.back_text = this.add.text(40, 50, "BACK", { fontSize: 54, color: "#FFFFFF" }).setVisible(false);
        this.treasure=this.physics.add.group();
+       this.back_text.setInteractive();
+       this.back_text.on('pointerdown', () => {
+           this.back_text.setVisible(false);
+           this.main_background.setVisible(true);
+           this.blur_background.setVisible(false);
+           this.frame_question.setVisible(false);
+           this.player.setVisible(true);
+           this.treasure.setVisible(true);
+           
+       })
        this.traes=[];
        this.traes_x=150;
        for(var i=0;i<4;i++){
-          var traes =this.treasure.create(this.traes_x,610,'treasure').setScale(0.15);
+          this.traes[i] =this.treasure.create(this.traes_x,610,'treasure').setScale(0.15);
           this.traes_x+=180;
        }
        this.player=this.physics.add.sprite(20,598,'player1');
@@ -43,7 +54,11 @@ class man2 extends Phaser.Scene {
         frameRate:5,
         repeat:-1
        })
-       this.physics.add.overlap(this.player,this.treasure,this.collision_trea,null,this);
+       for(var i=0;i<4;i++){
+        this.physics.add.overlap(this.player,this.traes[i],this.collision_trea,null,this);
+       }
+       
+
     }
     update() {
        var keyboard=this.input.keyboard.createCursorKeys();
@@ -61,7 +76,13 @@ class man2 extends Phaser.Scene {
        
     }
     collision_trea(player,trea){
-
+        //this.scene.start("man2");
+        this.blur_background.setVisible(true);
+        this.frame_question.setVisible(true);
+        player.setVisible(false);
+        trea.destroy();
+        this.treasure.setVisible(false);
+        this.back_text.setVisible(true);
     }
 
 }  
