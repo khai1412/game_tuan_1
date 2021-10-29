b1c42432db98cb9baf600a4f1fbfc639dae1505e
TuanVu <nguyentuanvu1395@gmail.com>
2021-10-29 22:15:49 +0700

    commit

--- src/scenes/man2.js
+++ src/scenes/man2.js
@@ -1,33 +1,16 @@
 class man2 extends Phaser.Scene {
-
   constructor() {
     super("man2");
   }
   preload() {
-    // VuNT ST
-    this.load.image("treasure", "assets/treasure.png");
-    this.load.image("game_frame_vu", "assets/game_frame.png");
-    // VuNT EN
+    this.load.image("main_background", "assets/background_scene_2.png");
+    this.load.image("blur_background", "assets/background_blur.jpg");
+    this.load.image("frame_question", "assets/game_frame.png");
   }
-  create() {}
-  update() {}
+  create() {
+    this.add
+      .image(0, 0, "main_background")
+      .setOrigin(0, 0)
+      .setScale(375 / 512, 375 / 256);
+  }
 }
-
-
-    constructor() {
-      super("man2");
-    }
-    preload(){
-       this.load.image('main_background','assets/background_scene_2.png');
-       this.load.image('blur_background','assets/background_blur.jpg');
-       this.load.image('frame_question','assets/game_frame.png');
-    }
-    create(){
-       this.add.image(0,0,'main_background').setOrigin(0,0).setScale(375/512,375/256);
-    }
-    update() {
-    
-    
-    }
-}  
-
