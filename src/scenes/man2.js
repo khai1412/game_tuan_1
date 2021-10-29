2e5a6448d0570f93b2dc212adf97217b710ad26e
khai1412 <khai1412@gmail.com>
2021-10-29 21:31:05 +0700

    xu ly man 2

--- src/scenes/man2.js
+++ src/scenes/man2.js
@@ -4,10 +4,12 @@ class man2 extends Phaser.Scene {
       super("man2");
     }
     preload(){
-        
+       this.load.image('main_background','assets/background_scene_2.png');
+       this.load.image('blur_background','assets/background_blur.jpg');
+       this.load.image('frame_question','assets/game_frame.png');
     }
     create(){
-
+       this.add.image(0,0,'main_background').setOrigin(0,0).setScale(375/512,375/256);
     }
     update() {
     
