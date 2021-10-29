817d84924db3b8b934eb525fd63c3383493accde
khai1412 <khai1412@gmail.com>
2021-10-29 21:49:45 +0700

    sua code biu lap man 2

--- src/scenes/man2.js
+++ src/scenes/man2.js
@@ -3,27 +3,18 @@ class man2 extends Phaser.Scene {
   constructor() {
     super("man2");
   }
-  preload() {
-    // VuNT ST
-    this.load.image("treasure", "assets/treasure.png");
-    this.load.image("game_frame_vu", "assets/game_frame.png");
-    // VuNT EN
-  }
-  create() {}
-  update() {}
-}
-
-
-    constructor() {
-      super("man2");
-    }
+  
     preload(){
+       this.load.image("treasure", "assets/treasure.png");
+       this.load.image("game_frame_vu", "assets/game_frame.png");
        this.load.image('main_background','assets/background_scene_2.png');
        this.load.image('blur_background','assets/background_blur.jpg');
        this.load.image('frame_question','assets/game_frame.png');
     }
     create(){
-       this.add.image(0,0,'main_background').setOrigin(0,0).setScale(375/512,375/256);
+       this.add.image(0,0,'main_background').setOrigin(0,0).setScale(375/512,375/256).setVisible(true);
+       this.add.image(0,0,'blur_background').setOrigin(0,0).setScale(2/3,375/374).setVisible(true);
+       
     }
     update() {
     
