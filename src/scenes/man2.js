8984243f0cd1970f8033f521537ca9a81e2ba8b2
TuanVu <nguyentuanvu1395@gmail.com>
2021-10-29 21:35:37 +0700

    Add image

--- src/scenes/man2.js
+++ src/scenes/man2.js
@@ -1,16 +1,13 @@
 class man2 extends Phaser.Scene {
-
-    constructor() {
-      super("man2");
-    }
-    preload(){
-        
-    }
-    create(){
-
-    }
-    update() {
-    
-    
-    }
-}  
+  constructor() {
+    super("man2");
+  }
+  preload() {
+    // VuNT ST
+    this.load.image("treasure", "assets/treasure.png");
+    this.load.image("game_frame_vu", "assets/game_frame.png");
+    // VuNT EN
+  }
+  create() {}
+  update() {}
+}
